905287ed7a09a3a75711e210b0c96113453b2735
WooJin_Chung <wjin9707@hufs.ac.kr>
2023-01-10 13:43:55 +0900

    Feat: mysql database connection

--- qs_project/qs_project/settings.py
+++ qs_project/qs_project/settings.py
@@ -13,8 +13,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 import json
 import modules
+import pymysql
 
 from pathlib import Path
+
+pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,10 +90,21 @@ WSGI_APPLICATION = 'qs_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER' : 'root',
+        'PASSWORD' : '12345678',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
