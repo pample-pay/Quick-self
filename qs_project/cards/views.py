15b825c9f07002cebb9cc66dc273a52b67e033d2
WooJin_Chung <wjin9707@hufs.ac.kr>
2023-01-30 17:25:56 +0900

    Feat: enroll user cards

--- qs_project/cards/views.py
+++ qs_project/cards/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
-from .models import Card_Info
+from .models import Card_Info, User_Card
 from .forms import CardEnrollForm
 
-from rest_framework import status
+from django.shortcuts import render, redirect
+from django.template.response import TemplateResponse
+
+from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 def enroll_view(request):
-    form = CardEnrollForm
-    return render(request, 'cards/enroll.html', {'form':form})
+
+    card_sets = User_Card.objects.filter(user_id = request.user)
+    context = {
+        'card_sets': card_sets
+    }
+
+    return render(request, 'cards/enroll.html',context)
 
 
 ### For API views ###
@@ -20,8 +27,11 @@ class CardEnrollView(APIView):
         try:
             cef = request.data
             card_num = cef['card_num1']+cef['card_num2']+cef['card_num3']+cef['card_num4']
+            
+            #####################
             card_token = card_num
-
+            #####################
+                
             point_num = ''
 
             if (cef['point_num1']!='' and
@@ -45,4 +55,108 @@ class CardEnrollView(APIView):
                 card_token=card_token,
                 defaults=updated_values
                 )
-            return Response({'message': 'OK'})
+
+            User_Card.objects.update_or_create(
+                user_id=request.user,
+                card_token=Card_Info.objects.get(card_token=card_token),
+            )
+            
+            card_sets = User_Card.objects.filter(user_id = request.user)
+            context = {
+                'card_sets': card_sets
+            }
+
+            return TemplateResponse(request, "cards/enroll.html", context)
+
+
+
+class CardInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card_Info
+        fields = [
+            'oiling_type',
+            'oiling_price',
+            'point_token',
+            'oiling_receipt',
+        ]
+
+
+class CardCheckView(APIView):
+
+    def post(self, request):
+
+        try:
+            ccf = request.data
+            card_num = ccf['card_num1']+ccf['card_num2']+ccf['card_num3']+ccf['card_num4']
+
+            #####################
+            card_token = card_num
+            #####################
+
+            card_info = ''
+
+            card_info = Card_Info.objects.get(card_token = card_token)
+            serializer_class = CardInfoSerializer(card_info)
+
+            context = serializer_class.data
+        
+            context['card_num1'] = ccf['card_num1']
+            context['card_num2'] = ccf['card_num2']
+            context['card_num3'] = ccf['card_num3']
+            context['card_num4'] = ccf['card_num4']
+
+            context['oiling_type'] = Card_Info(oiling_type=context['oiling_type']).get_oiling_type_display()
+            context['oiling_price'] = Card_Info(oiling_price=context['oiling_price']).get_oiling_price_display()
+            context['oiling_receipt'] = Card_Info(oiling_receipt=context['oiling_receipt']).get_oiling_receipt_display()
+
+            context['point_token'] = '없음'
+            if card_info.point_token:
+                context['point_token'] = card_info.point_token
+
+        except KeyError:
+            return Response({'message': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        else:
+            card_sets = User_Card.objects.filter(user_id = request.user)
+            context['card_sets']= card_sets
+
+            return TemplateResponse(request, "cards/enroll.html", context)
+
+
+
+class CardCheckEnrollView(APIView):
+
+    def post(self, request):
+
+        try:
+            ccf = request.data
+            card_num = ccf['card_num1']+ccf['card_num2']+ccf['card_num3']+ccf['card_num4']
+
+            ####
+            card_token = card_num
+            ####
+
+
+        except KeyError:
+            return Response({'message': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        else:
+            User_Card.objects.update_or_create(
+                user_id=request.user,
+                card_token=Card_Info.objects.get(card_token=card_token),
+            )
+
+            card_sets = User_Card.objects.filter(user_id = request.user)
+            context = {
+                'card_sets': card_sets
+            }
+
+            return TemplateResponse(request, "cards/enroll.html", context)
+
+class CardDelete(APIView):
+
+    def post(self, request, pk):
+        card_post = User_Card.objects.get(pk=pk)
+        card_post.delete()   
+        
+        return redirect('/enroll')
