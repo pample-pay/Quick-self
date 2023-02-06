1c22423ac8f0311d186456e4ac8575eefbf72adc
WooJin_Chung <wjin9707@hufs.ac.kr>
2023-02-06 18:12:44 +0900

    Feat: get_token

--- qs_project/modules.py
+++ qs_project/modules.py
@@ -1,3 +1,7 @@
+import socket
+
+from datetime import datetime
+
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
@@ -8,3 +12,74 @@ def get_secret(setting, secrets):
     except KeyError:
         error_msg = "Set the {0} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
+
+def get_token(card_num):
+    HOST = '211.33.136.19'
+    PORT = 23271
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+
+    Space = " "*150
+    card_num
+    CardData = "37" + card_num + Space[0:127 - 2 - len("4009131145851610=2506")]
+    CardData = "37" + "4009131145851610=2506" + Space[0:127 - 2 - len("4009131145851610=2506")]
+
+    date = datetime.today().strftime("%Y%m%d")[2:]
+    time = datetime.today().strftime("%H%M%S")   
+
+    SendData = [
+        "S-OILQS"
+        + "23933000010112110830"
+        + "0200"
+        + "10"   
+        + "2393300001"
+        + Space[0:2]
+        + Space[0:2]
+        + date
+        + time
+        + "@"
+        + CardData
+        + "00"
+        + "000000000000"
+        + "000000000000"
+        + "000000000000"
+        + Space[0:16]
+        + Space[0:12]
+        + Space[0:6]
+        + Space[0:1]
+        + Space[0:8]
+        + Space[0:8]
+        + Space[0:1]
+        + Space[0:1]
+        + Space[0:6]
+        + Space[0:12]
+        + Space[0:8]
+        + Space[0:5]
+        + Space[0:1]
+        + Space[0:127]
+        + Space[0:1]
+        + Space[0:12]
+        + Space[0:1]
+        + Space[0:12]
+        + Space[0:3]
+        + Space[0:24]
+        + "################"
+        + "################"
+        + Space[0:2]
+    ]
+
+    SendData.insert(0, str(len(SendData[0])).zfill(4))
+
+    bytes = (SendData[0]+SendData[1]).encode()
+
+    s.send(bytes)
+    data = s.recv(4096)
+    s.close()
+
+    recvBuff = data[422:500]
+
+    card_token39 = recvBuff[:39]
+    card_token05 = recvBuff[39:]
+
+    return card_token39, card_token05
