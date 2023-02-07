58f2a005d7c8c39ce1ba26cadae38821049a0f9d
WooJin_Chung <wjin9707@hufs.ac.kr>
2023-02-07 18:26:41 +0900

    Feat: login page

--- qs_project/users/urls.py
+++ qs_project/users/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.index_view, name='index_view'),
-
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('', views.LoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
     
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
