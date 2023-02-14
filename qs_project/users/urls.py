3f5d8c597c33686141551f4d1ebe8e9816956d48
WooJin_Chung <wjin9707@hufs.ac.kr>
2023-02-14 18:15:28 +0900

    Feat: users chage

--- qs_project/users/urls.py
+++ qs_project/users/urls.py
@@ -6,8 +6,9 @@ app_name = 'users'
 urlpatterns = [
     path('', views.LoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
-    
+        
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
     path('api/v1/id-validation', views.IdValidation.as_view(), name='auth_message'), 
+    path('api/v1/hp-validation', views.HPValidation.as_view(), name='auth_message'), 
     path('api/v1/auth-message', views.AuthView.as_view(), name='auth_message'), 
 ]
