ff0064255eaa40fba68289c8413483bb644e84ab
WooJin_Chung <wjin9707@hufs.ac.kr>
2023-02-27 14:17:13 +0900

    Feat : wsgi static folder

--- qs_project/qs_project/wsgi.py
+++ qs_project/qs_project/wsgi.py
@@ -16,5 +16,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qs_project.settings')
 
 application = get_wsgi_application()
 
-# application = WhiteNoise(application, root='')
+application = WhiteNoise(application, root='')
 application.add_files('/web/qs_project/.static', prefix='')
+# application.add_files('./static', prefix='')
