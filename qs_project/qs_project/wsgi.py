83c7c492cd9a2a200fe3651fd0d8470b010f68d5
KwonYoungDeok <pamplepay@gamil.com>
2023-04-21 10:56:06 +0900

    2023.04.21 netalf wsgi comment commit

--- qs_project/qs_project/wsgi.py
+++ qs_project/qs_project/wsgi.py
@@ -18,3 +18,4 @@ application = get_wsgi_application()
 
 application = WhiteNoise(application, root='')
 application.add_files('/web/qs_project/.static', prefix='')
+# application.add_files('./static', prefix='')
